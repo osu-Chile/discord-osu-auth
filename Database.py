c36af4b7ea8815f598997438e0414368ca235ff2
Francisco Hauva <phauva@gmail.com>
2022-05-24 00:44:32 -0400

    Fetch data before closing db connection.

--- Database.py
+++ Database.py
@@ -8,47 +8,49 @@ def db_connect():
         host=os.environ["DB_HOST"],
         database=os.environ["DB_NAME"],
         user=os.environ["DB_USERNAME"],
-        password=os.environ["DB_PASSWORD"],
-        auth_plugin='mysql_native_password'
+        password=os.environ["DB_PASSWORD"]
     )
 
 
 def select_by_discord_id(discord_id: int):
-    sql = f"SELECT * FROM users WHERE discordid = '{discord_id}'"
+    sql = f"SELECT * FROM users WHERE discord_id = {discord_id}"
     db_connection = db_connect()
     cursor = db_connection.cursor()
     cursor.execute(sql)
+    data = cursor.fetchone()
     db_connection.close()
-    return cursor.fetchone()
+    return data
 
 
 def select_by_osu_id(osu_id: int):
-    sql = f"SELECT * FROM users WHERE osuid = '{osu_id}'"
+    sql = f"SELECT * FROM users WHERE osu_id = {osu_id}"
     db_connection = db_connect()
     cursor = db_connection.cursor()
     cursor.execute(sql)
+    data = cursor.fetchone()
     db_connection.close()
-    return cursor.fetchone()
+    return data
 
 
 def insert_user(discord_name: str, osu_username: str, discord_tag: int, medals: int, osu_id: int, discord_id: int):
-    sql = "INSERT INTO users (`id`, `discordname`, `osuname`, `discordtag`, `medals`, `osuid`, `discordid`) " \
-          f"VALUES (NULL, '{discord_name}', '{osu_username}', {discord_tag}, {medals}, {osu_id}, {discord_id});"
-    print(sql)
+    sql = "INSERT INTO users (id, discord_name, osu_name, discord_tag, medals, osu_id, discord_id) " \
+          f"VALUES (NULL, '{discord_name}', '{osu_username}','{discord_tag}', {medals}, {osu_id}, {discord_id});"
     db_connection = db_connect()
     cursor = db_connection.cursor()
     cursor.execute(sql)
     db_connection.commit()
+    rows = cursor.rowcount
     db_connection.close()
-    return cursor.rowcount
+    return rows
 
 
-def update_user(discord_id: int, discord_username: str, discord_discriminator: str, osu_username: str):
-    sql = f"UPDATE users SET discordname='{discord_username}', discordtag='{discord_discriminator}', " \
-          f"osuname='{osu_username}' WHERE discordid = '{discord_id}';"
+def update_user(discord_id: int, discord_username: str, discord_tag: str, osu_username: str):
+    sql = f"UPDATE users SET discord_name='{discord_username}', discord_tag='{discord_tag}', " \
+          f"osu_name='{osu_username}' WHERE discord_id = {discord_id};"
     db_connection = db_connect()
     cursor = db_connection.cursor()
     cursor.execute(sql)
     db_connection.commit()
+    rows = cursor.rowcount
     db_connection.close()
-    return cursor.rowcount
+    return rows
