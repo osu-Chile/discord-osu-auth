e4649818333ed44953b3f0506f2b332f8c284294
Francisco Hauva <phauva@gmail.com>
2022-06-08 01:58:28 -0400

    Implement usage of parameterized queries.

--- Database.py
+++ Database.py
@@ -6,60 +6,74 @@ def db_connect():
 
 
 def select_by_discordid(discordid: int):
-    sql = f"SELECT * FROM users WHERE discordid = {discordid}"
+    parameters = [discordid]
+    sql = f"SELECT * FROM users WHERE discordid = %s"
+
     db_connection = db_connect()
     cursor = db_connection.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, parameters)
     data = cursor.fetchone()
+
     cursor.close()
     db_connection.close()
     return data
 
 
 def select_by_osuid(osuid: int):
-    sql = f"SELECT * FROM users WHERE osuid = {osuid}"
+    parameters = [osuid]
+    sql = f"SELECT * FROM users WHERE osuid = %s"
+
     db_connection = db_connect()
     cursor = db_connection.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, parameters)
     data = cursor.fetchone()
+
     cursor.close()
     db_connection.close()
     return data
 
 
 def insert_user(discordname: str, osuname: str, discordtag: str, medals: int, osuid: int, discordid: int):
+    parameters = [discordname, osuname, discordtag, medals, osuid, discordid]
     sql = "INSERT INTO users (id, discordname, osuname, discordtag, medals, osuid, discordid) " \
-          f"VALUES (NULL, '{discordname}', '{osuname}','{discordtag}', {medals}, {osuid}, {discordid});"
+          "VALUES (NULL, %s, %s, %s, %s, %s, %s);"
+
     db_connection = db_connect()
     cursor = db_connection.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, parameters)
     db_connection.commit()
     rows = cursor.rowcount
+
     cursor.close()
     db_connection.close()
     return rows
 
 
 def update_user(discordid: int, discordname: str, discordtag: str, osuname: str):
-    sql = f"UPDATE users SET discordname='{discordname}', discordtag='{discordtag}', " \
-          f"osuname='{osuname}' WHERE discordid = {discordid};"
+    parameters = [discordname, discordtag, osuname, discordid]
+    sql = "UPDATE users SET discordname = %s, discordtag = %s, osuname = %s WHERE discordid = %s;"
+
     db_connection = db_connect()
     cursor = db_connection.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, parameters)
     db_connection.commit()
     rows = cursor.rowcount
+
     cursor.close()
     db_connection.close()
     return rows
 
 
 def delete_by_userid(discordid: int):
-    sql = f"DELETE FROM users WHERE discordid = {discordid}"
+    parameters = [discordid]
+    sql = f"DELETE FROM users WHERE discordid = %s"
+
     db_connection = db_connect()
     cursor = db_connection.cursor()
-    cursor.execute(sql)
+    cursor.execute(sql, parameters)
     db_connection.commit()
     rows = cursor.rowcount
+
     cursor.close()
     db_connection.close()
     return rows
